c8befdb1308a6fbb10e562b3112b8e83d265ee97
Daniel Porto <danielsalesporto@gmail.com>
2026-06-03 21:47:37 -0300

    Consumer

---
+++ src/main/java/com/redhat/EventConsumer.java
@@ -0,0 +1,14 @@
+package com.redhat;
+import jakarta.enterprise.context.ApplicationScoped;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.jboss.logging.Logger;
+
+@ApplicationScoped
+public class EventConsumer {
+    private static final Logger LOG = Logger.getLogger(EventConsumer.class);
+
+    @Incoming("incoming-events")
+    public void consume(String message) {
+        LOG.infof("[CONSUMER] Evento recebido: %s", message);
+    }
+}
